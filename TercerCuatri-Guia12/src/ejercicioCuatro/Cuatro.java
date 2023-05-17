2296cf256c79ed10733f347dba1a01dd20a023b1
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-17 10:40:20 -0300

    Ejercicio cuatro
    Crear un programa que lea dos archivos de texto y escriba el contenido de ambos archivos en un nuevo archivo de texto.

---
+++ TercerCuatri-Guia12/src/ejercicioCuatro/Cuatro.java
@@ -0,0 +1,10 @@
+package ejercicioCuatro;
+
+public class Cuatro {
+	public static void main(String[] args) {
+		
+		String rutaPrimerArchivo = "C:\\Users\\Alumno\\Downloads\\ejercicioCuatro\\archivoUno.txt";
+		
+	}
+
+}
